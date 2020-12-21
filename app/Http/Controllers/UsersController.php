f5f5665124a44393c1d160ca161dedcd702086d1
alexchen <alexchen@abdstem.com>
2020-12-21 17:30:06 +0800

    登录后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -17,6 +18,9 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    /**
+     * 注册
+     */
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -30,6 +34,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        Auth::login($user);
         session()->flash('success', '欢迎使用laravel-demo');
         return redirect(route('users.show', [$user->id]));
     }
