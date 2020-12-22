0e7987a1b9c55c1d06664a66bde8a047f730ac7f
alexchen <alexchen@abdstem.com>
2020-12-22 14:14:08 +0800

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,20 +7,36 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        //只有未登陆的用户可以访问登陆页面
+//        $this->middleware('guest', [
+//            'only' => 'create'
+//        ]);
+    }
+
     public function create()
     {
+        //已登录的用直接跳转到用户页面
+        if (Auth::user()) {
+            session()->flash('info', Auth::user()->name.' 欢迎回来！');
+            return redirect()->intended();
+        }
         return view('sessions.create');
     }
 
     public function store(Request $request)
     {
+        if (Auth::user()) return redirect()->intended(route('users.show',[Auth::user()]));
+
         $credentials = $this->validate($request,[
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            $fallback = route('users.show',[Auth::user()]);
+            return redirect()->intended($fallback);//重定向到上次访问的页面，如果没有则回到用户信息页面
         } else {
             session()->flash('danger', '很抱歉，邮箱和密码不匹配');
             return redirect()->back()->withInput();
