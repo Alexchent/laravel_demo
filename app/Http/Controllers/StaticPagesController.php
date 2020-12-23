b8642b376dae18bc33eb3b7b46913280712e43cd
alexchen <alexchen@abdstem.com>
2020-12-23 18:52:56 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return View('home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(5);
+        }
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
     {
-
+        return view('static_pages/help');
     }
 
     public function about()
     {
-
+        return view('static_pages/about');
     }
 }
