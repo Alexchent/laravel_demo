bc45b0a0ad8a8b427aa0f5d7562b5196f6589352
alexchen <alexchen@abdstem.com>
2020-12-24 18:11:01 +0800

    关注和取关

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,6 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store','destroy']
 
 Route::get('/users/{user}/followings', [\App\Http\Controllers\UsersController::class, 'followings'])->name('users.followings');
 Route::get('/users/{user}/followers', [\App\Http\Controllers\UsersController::class, 'followers'])->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
